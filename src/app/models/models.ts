03cea545aaf4bc0d978ecb40d4803942c6a4b51a
Krishnanperiysamy <115148209+Krishnanperiysamy@users.noreply.github.com>
2023-04-25 12:14:30 +0530

    initial commit

---
+++ src/app/models/models.ts
@@ -0,0 +1,12 @@
+export interface Category{
+    id: number;
+    category: string;
+    subcategory: string;
+}
+
+export interface SuggestedProduct{
+
+    banerimage : string;
+    category : Category;
+
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+  @Input() view : 'grid'| 'list'|'currcartitem'|'prevcartitem'='grid'
+
+  constructor(){}
+
+  ngOnInit():void{}
+
+}
